ed02fe6c7ae9c861f62ab6a778eff4e0a3c94310
jakkapat-tg <75299815+jakkapat-tg@users.noreply.github.com>
2021-11-23 13:52:52 +0700

    admin

---
+++ public/page/form/01from_request_salary_admin.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="th">
+<?php require "./public/components/head.php" ?>
+
+<body>
+    <?php
+    $page = 'form';
+    include "./public/components/navbar.php";
+    $formname = 'หนังสือรับรองเงินเดือน';
+    $form_id = '001';
+    require("./service/formmanage.php");
+    $obj  = new manage_form();
+
+    if (isset($_POST['submit'])) {
+        $note =   $_POST['note'];
+        $insert =  $obj->insert($form_id, $_SESSION['user_id'], $_SESSION['person_id'], $formname, $_SESSION['fullname'], $note);
+        if ($insert === true) {
+            echo '<script>
+                    Swal.fire({
+                        title: "เพิ่มข้อมูลสำเร็จ!",
+                        text: "Insert data successfuly!",
+                        type: "success"
+                    }).then(function() {
+                        window.location = "./form_request_salary";
+                    });
+                    </script>';
+            $errormesssage = null;
+        } else {
+            $errormesssage =  "<p class='mt-2 alert alert-danger'> " . $insert . '</p>';
+        }
+    }
+    ?>
+
+    <div class="container">
+        <!-- <div class="card mt-5">
+            <div class="card-body">
+                <h5>แบบฟอร์มขอใบรับรองเงินเดือน</h5>
+                <form action="#" method="post">
+                    <p>โปรดกรอกหมายเหตุที่ต้องการขอใบรับรอง</p>
+                    <input class="form-control" type="text" name="note" placeholder="ระบุเหตุผลที่ต้องการขอใบรับรอง" value="" required>
+                    <button type="submit" name="submit" value="submit" class="btn btn-success btn-lg mt-3">บันทึก</button>
+                    <a href="./form" class="btn btn-secondary btn-lg mt-3">ย้อนกลับ</a>
+                </form>
+
+                <?php
+
+                // if (isset($errormesssage)) echo $errormesssage; 
+                ?>
+            </div>
+        </div> -->
+
+        <hr>
+        <h2>รายการขอใบรับรองเงินเดือนของท่าน</h2>
+        <p class="text-light">ผู้ใช้งานนะ <?php echo $_SESSION['fullname']; ?></p>
+        <div class="card">
+            <div class="card-body">
+                <table id="example1" class="table table-bordered table-striped">
+                    <thead>
+                        <th>วันเวลาที่ขอ</th>
+                        <th>หมายเหตุ</th>
+                        <th>สถานะ</th>
+                    </thead>
+
+                    <tbody>
+                        <?php
+                        $sql =  $obj->fetct_byuser($_SESSION['user_id']);
+                        while ($row = mysqli_fetch_array($sql)) {
+                        ?>
+                            <tr>
+                                <td><?php echo $row['timestamp'] ?></td>
+                                <td><?php echo $row['note'] ?></td>
+                                <td><?php echo $row['status'] == null ? 'รอดำเนินการ' : $row['status'] == 1 ? 'ผ่านการอนุมัติ' : 'ไม่ผ่านการอนุมัติ'; ?></td>
+                                <!-- <td>
+                                    <a href="./print_salary?<?php //echo " "; 
+                                                            ?>">
+                                        <button class="btn btn-warning btn-block"><i class="fa fa-print" aria-hidden="true"></i>
+                                        </button>
+                                    </a>
+                                </td> -->
+                            </tr>
+                        <?php } ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+
+    </div>
+
+
+
+
+
+    <?php include './public/components/footer.php'; ?>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+
+</html>
