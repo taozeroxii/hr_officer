89c0d4669c72ef9b66310c3517ecf1817f32b346
jakkapat-tg <75299815+jakkapat-tg@users.noreply.github.com>
2021-11-24 14:23:08 +0700

    submit

--- public/page/form/post.php
+++ public/page/form/post.php
@@ -3,6 +3,5 @@
     require "./service/formmanage.php";
     $obj  = new manage_form();
     $sql = $obj->approve($_POST['id'], $_POST['status']);
-    echo $_POST['id'];
-    echo $_POST['status'];
-    header("location: ./form_request_salary_admin");
+    if ($sql)
+        header("location: ./form_request_salary_admin");
