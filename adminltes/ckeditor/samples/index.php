7c3dc4e21ae8f626741b45c2c3540a95c3c41188
eaktamp <eaktamp@gmail.com>
2021-11-26 09:28:30 +0700

    ckeditor

---
+++ adminltes/ckeditor/samples/index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>CKEditor Sample</title>
+	<script src="../ckeditor.js"></script>
+	<script src="js/sample.js"></script>
+	<link rel="stylesheet" href="css/samples.css">
+	<link rel="stylesheet" href="toolbarconfigurator/lib/codemirror/neo.css">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<meta name="description" content="Try the latest sample of CKEditor 4 and learn more about customizing your WYSIWYG editor with endless possibilities.">
+<style>
+
+
+
+</style>
+
+
+
+</head>
+<body id="main">
+
+<main>
+
+	<div class="adjoined-bottom">
+		<div class="grid-container">
+			<div class="grid-width-100">
+				<div id="editor">
+<p>&nbsp;</p>
+
+<p style="margin-left:40px">
+<span style="display:none">&nbsp;</span><span style="font-family:Trebuchet MS,Helvetica,sans-serif"><span style="font-size:16px"><strong><img src="img/krut-3-cm.PNG" style="height:80px; width:80px" />&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</strong></span><span style="font-size:36px"><strong>บันทึกข้อความ</strong></span></span></p>
+
+<p style="margin-left:40px">
+<span style="font-family:Trebuchet MS,Helvetica,sans-serif"><span style="font-size:16px"><strong>ส่วนราชการ</strong><u> &nbsp;&nbsp;โรงพยาบาลเจ้าพระยาอภัยภูเบศร&nbsp; (กลุ่มงานทรัพยากรบุคคล โทร.2503/037217128)&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</u></span></span></p>
+
+<p style="margin-left:40px">
+<span style="font-family:Trebuchet MS,Helvetica,sans-serif"><span style="font-size:16px"><strong>ที่</strong><u>&nbsp;&nbsp; ปจ 0032.101.04/พิเศษ<strong>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;</strong></u><strong>วันที่</strong><u>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</u></span></span></p>
+
+<p style="margin-left:40px">
+<span style="font-family:Trebuchet MS,Helvetica,sans-serif"><span style="font-size:16px"><strong>เรื่อง</strong><u>&nbsp;&nbsp; ขอหนังสือรับรอง&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;</u></span></span></p>
+
+<p style="margin-left:40px">
+<span style="font-family:Trebuchet MS,Helvetica,sans-serif"><span style="font-size:16px">เรียน&nbsp;&nbsp;&nbsp; ผู้อำนวยการโรงพยาบาลเจ้าพระยาอภัยภูเบศร</span></span></p>
+
+<p style="margin-left:40px">
+<span style="font-family:Trebuchet MS,Helvetica,sans-serif"><span style="font-size:16px">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></span></p>
+
+<p style="margin-left:40px">
+<span style="font-family:Trebuchet MS,Helvetica,sans-serif"><span style="font-size:16px">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; ด้วยข้าพเจ้า (นาย/นาง/นางสาว).............................................................................................</span></span></p>
+
+<p style="margin-left:40px">
+<span style="font-family:Trebuchet MS,Helvetica,sans-serif"><span style="font-size:16px">ตำแหน่ง............................................................... ฝ่าย/กลุ่มงาน...........................................................</span></span></p>
+
+<p style="margin-left:40px">
+<span style="font-family:Trebuchet MS,Helvetica,sans-serif"><span style="font-size:16px">โรงพยาบาลเจ้าพระยาอภัยภูเบศร&nbsp; มีความประสงค์ขอหนังสือรับรองเพื่อ......................................................................</span></span></p>
+
+<p style="margin-left:40px">
+<span style="font-family:Trebuchet MS,Helvetica,sans-serif"><span style="font-size:16px">...................................................................................................................................................</span></span></p>
+
+<p style="margin-left:40px">
+<span style="font-family:Trebuchet MS,Helvetica,sans-serif"><span style="font-size:16px"><strong><u>แนบสลิปเงินเดือนปัจจุบัน 1 ฉบับ หมายเลขโทรศัพท์ เบอร์ภายใน/มือถือ</u></strong>..............................................................</span></span></p>
+
+<p>&nbsp;</p>
+
+<p><span style="font-family:Trebuchet MS,Helvetica,sans-serif"><span style="font-size:16px">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; จึงเรียนมาเพื่อโปรดพิจารณาอนุมัติ</span></span></p>
+
+<p>&nbsp;</p>
+
+<p>&nbsp;</p>
+
+<p style="text-align:center"><span style="font-family:Trebuchet MS,Helvetica,sans-serif"><span style="font-size:16px">&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;.</span></span></p>
+
+<p style="text-align:center"><span style="font-family:Trebuchet MS,Helvetica,sans-serif"><span style="font-size:16px">(...........................................................)</span></span></p>
+
+<p style="text-align:center"><span style="font-family:Trebuchet MS,Helvetica,sans-serif"><span style="font-size:16px">ตำแหน่ง.............................................................</span></span></p>
+
+<p>&nbsp;</p>
+
+<p style="margin-left:40px"><span style="font-family:Trebuchet MS,Helvetica,sans-serif"><span style="font-size:16px">เรียน&nbsp;&nbsp;&nbsp; ผอก.&nbsp; (ผ่าน รองฯ บริหาร)</span></span></p>
+
+<p style="margin-left:40px"><span style="font-family:Trebuchet MS,Helvetica,sans-serif"><span style="font-size:16px">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; เพื่อทราบและลงนามรับรอง</span></span></p>
+
+<p>&nbsp;</p>
+
+<p style="text-align:center"><span style="font-family:Trebuchet MS,Helvetica,sans-serif"><span style="font-size:16px"><strong>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;</strong></span></span></p>
+
+<p>&nbsp;</p>
+
+<p style="text-align:center"><span style="font-family:Trebuchet MS,Helvetica,sans-serif"><span style="font-size:16px"><span style="color:white">ข้อความ</span></span></span></p>
+
+<p><span style="font-family:Trebuchet MS,Helvetica,sans-serif"><span style="font-size:16px"><strong><span style="color:white">ส่วนรา</span></strong></span></span></p>
+
+<p>&nbsp;</p>
+
+<p>&nbsp;</p>
+
+<p><span style="display:none">&nbsp;</span>&nbsp;</p>
+
+<p>&nbsp;</p>
+
+
+
+				</div>
+			</div>
+		</div>
+	</div>
+
+</main>
+<script>
+	initSample();
+</script>
+
+</body>
+</html>
